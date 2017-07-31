42c7a15c29b4e5033de2c609aeea5c2760e13f29
Simon Vrachliotis <simon@simonswiss.com>
2017-07-31 15:23:10 +1000

    converted to styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+// import "./App.css";
 
-// the person card
+import styled, { injectGlobal } from "styled-components";
+
+// eslint-disable-next-line
+injectGlobal`
+  body {
+    margin: 0;
+    font-family: -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Helvetica,
+      Arial,
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    }
+`;
+
+const Card = styled.div`
+  padding: 1.75rem;
+  margin: .5rem;
+  border-radius: 4px;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
+  color: white;
+
+  background: ${props => (props.sex === "male" ? "#44bccc" : "#f973bc")};
+`;
+
+const Name = styled.h2`
+  margin-top: 0;
+  font-weight: 900;
+  margin-bottom: .75rem;
+`;
+
+const Bio = styled.p`
+  margin: 0;
+  line-height: 1.5;
+  strong {
+    font-weight: 900;
+  }
+`;
+
 const Person = props =>
-  <div className={`person person--${props.sex}`}>
-    <h2 className="person__name">{props.name}</h2>
-    <p className="person__description">
+  <Card sex={props.sex}>
+    <Name>{props.name}</Name>
+    <Bio>
       This <strong>{props.sex}</strong> is currently{" "}
       <strong>{props.age} years old</strong>.
-    </p>
-  </div>;
+    </Bio>
+  </Card>;
 
 const people = [
   { name: "Anna", sex: "female", age: 28 },
