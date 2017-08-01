fafe9d167b1366e92d897a7405789c822caa91bf
Simon Vrachliotis <simon@simonswiss.com>
2017-08-01 10:25:49 +1000

    cleaning up the starting point

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 
-// the person card
-const Person = props =>
-  <div className={`person person--${props.sex}`}>
-    <h2 className="person__name">{props.name}</h2>
-    <p className="person__description">
-      This <strong>{props.sex}</strong> is currently{" "}
-      <strong>{props.age} years old</strong>.
-    </p>
-  </div>;
-
+/* ======================= */
+/* ===== sample data ===== */
+/* ======================= */
 const people = [
   { name: "Anna", sex: "female", age: 28 },
   { name: "John", sex: "male", age: 31 },
@@ -18,7 +11,9 @@ const people = [
   { name: "Bella", sex: "female", age: 4 }
 ];
 
-// the list of people
+/* ============================== */
+/* ===== the main component ===== */
+/* ============================== */
 const App = () =>
   <div>
     {people.map((person, i) => {
@@ -26,6 +21,18 @@ const App = () =>
         <Person name={person.name} age={person.age} sex={person.sex} key={i} />
       );
     })}
+  </div>;
+
+/* =========================== */
+/* ===== the Person card ===== */
+/* =========================== */
+const Person = props =>
+  <div className={`person person--${props.sex}`}>
+    <h2 className="person__name">{props.name}</h2>
+    <p className="person__description">
+      This <strong>{props.sex}</strong> is currently{" "}
+      <strong>{props.age} years old</strong>.
+    </p>
   </div>;
 
 export default App;
